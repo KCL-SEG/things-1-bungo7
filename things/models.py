6d9a3f75ff289161f2766df592c90c726bdcaa23
bungo7 <allen.kallikadan_rapheal@kcl.ac.uk>
2022-12-09 12:35:36 +0000

    Revert "add model"
    This reverts commit 2672c8e7bed36932950550381705ca31a7b62bb7.

--- things/models.py
+++ things/models.py
@@ -1,11 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.core.validators import MaxValueValidator, MinValueValidator
 
-class Thing(AbstractUser):
-    name = models.CharField(unique=True, blank=False,max_length=30)
-    description = models.CharField(unique=False, blank=True, max_length=120)
-    quantity = models.IntegerField(unique=False, blank=True, validators=[
-            MaxValueValidator(100),
-            MinValueValidator(0)
-        ])
+# Create your models here.
